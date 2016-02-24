4b7664988bdf37cdae0e02121e143a50049254b4
richardlacap <richardlacap@gmail.com>
2016-02-24 16:18:48 -0600

    Initial Commit

---
+++ NPocoIdentityWebAppTemplate/Data_Access/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NPoco;
+using Pacal.NPoco_Idenity_Provider;
+
+namespace NPocoIdentityWebAppTemplate.Data_Access
+{
+    public class ApplicationDbContext : IDisposable
+    {
+        public DataProvider Database { get; private set; }
+        
+        public ApplicationDbContext()
+        {
+            var connectionString =  System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            Database = new DataProvider(connectionString, DatabaseType.SqlServer2012);
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        public ApplicationDbContext Get()
+        {
+            return Database != null ? this : new ApplicationDbContext();
+        }
+
+        public void Dispose()
+        {
+            Database.Dispose();
+            Database = null;
+        }
+    }
+}
---
+++ NPocoIdentityWebAppTemplate/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NPocoIdentityWebAppTemplate")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NPocoIdentityWebAppTemplate")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cbebf1e6-73e6-45bb-bc5d-99456dde2960")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
