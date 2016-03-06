917a2180223677162bded4f7b25998197bdaa8c3
richardlacap <richardlacap@gmail.com>
2016-03-06 12:33:41 -0600

    re issue #1 - fix missing setter on MyCustomUser

--- NPocoIdentityWebAppTemplate/Models/MyCustomUser.cs
+++ NPocoIdentityWebAppTemplate/Models/MyCustomUser.cs
@@ -25,7 +25,7 @@ namespace NPocoIdentityWebAppTemplate.Models
             UserName = userName;
         }
 
-        public string Id { get; }
+        public string Id { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
         public bool EmailConfirmed { get; set; }
